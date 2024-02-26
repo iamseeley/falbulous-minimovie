cf082835198337863e2cb14882e76b9435556e60
iamseeley <seeleypersonal@gmail.com>
2024-02-26 13:59:47 -0500

    ffmpeg working, but it's finicky

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
           </h2>
           <h4 className='text-xl font-semibold'>Write a story scene by scene and create a mini movie!</h4>
           <div className='flex flex-row gap-2 my-4'>
-            <Link target='_blank' href={"https://fal.ai"} className='hover:bg-gray-300 py-2 px-4 bg-gray-100 font-semibold'>fal.ai</Link><Link target='_blank' href={"https://github.com/iamseeley/falbalous-minimovie"} className='hover:bg-gray-300 font-semibold py-2 px-4 bg-gray-100'>source</Link>
+            <Link target='_blank' href={"https://fal.ai"} className='hover:bg-gray-300 py-2 px-4 bg-gray-100 font-semibold'>fal.ai</Link><Link target='_blank' href={"https://github.com/iamseeley/falbulous-minimovie"} className='hover:bg-gray-300 font-semibold py-2 px-4 bg-gray-100'>source</Link>
           </div>
         </div>
         
