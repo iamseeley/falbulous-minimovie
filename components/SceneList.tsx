2f17ba2b526c1efec0c3b34964edbe33145f8a65
iamseeley <seeleypersonal@gmail.com>
2024-02-29 16:49:31 -0500

    working on scene carousel

--- components/SceneList.tsx
+++ components/SceneList.tsx
@@ -49,10 +49,10 @@ const SceneList: React.FC<SceneListProps> = ({ scenes, scenesInfo }) => {
         </div>
       </div>
       <div className="flex justify-center gap-2 mt-4">
-        <button onClick={handlePrev} className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded disabled:opacity-60">
+        <button onClick={handlePrev} className="bg-black hover:opacity-80 text-white font-bold py-2 px-4 rounded disabled:opacity-60">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6"/></svg>
         </button>
-        <button onClick={handleNext} className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded disabled:opacity-60">
+        <button onClick={handleNext} className="bg-black hover:opacity-80 text-white font-bold py-2 px-4 rounded disabled:opacity-60">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"/></svg>
         </button>
       </div>
--- components/ui/SceneNavigation.tsx
+++ components/ui/SceneNavigation.tsx
@@ -21,7 +21,7 @@ interface SceneNavigationProps {
       {currentSceneIndex > 0 && (
         <button
           onClick={moveToPreviousScene}
-          className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded disabled:opacity-60"
+          className="bg-black hover:opacity-80 text-white font-bold py-2 px-4 rounded disabled:opacity-60"
           disabled={loading || !scenesInfo[scenes[currentSceneIndex - 1]]?.url}
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6"/></svg>
@@ -30,7 +30,7 @@ interface SceneNavigationProps {
       {currentSceneIndex < scenes.length - 1 && (
         <button
           onClick={moveToNextScene}
-          className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded disabled:opacity-60"
+          className="bg-black hover:opacity-80 text-white font-bold py-2 px-4 rounded disabled:opacity-60"
           disabled={loading || !scenesInfo[scenes[currentSceneIndex]]?.url}
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"/></svg>
