30cef7d4803f4aec3efbe6f3ee323eb2ef56b6af
iamseeley <seeleypersonal@gmail.com>
2024-08-20 15:45:32 -0400

    adding tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             {children}
           </main>
         <Footer />
+        <Script src="https://esm.town/v/iamseeley/trackingScript" />
       </body>
     </html>
   );
